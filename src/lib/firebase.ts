035b8dffeedf53cf2499cc05097ba51973346bcf
Subrata Mishra <subratamishra213@gmail.com>
2025-09-01 19:29:57 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 // This is a placeholder for the Firebase config.
 // In a real application, you would get this from the Firebase Console.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "thumbgenius-pz762",
+  "appId": "1:675902510186:web:2574aefee340d51ce1e613",
+  "storageBucket": "thumbgenius-pz762.firebasestorage.app",
+  "apiKey": "AIzaSyB7NL8TmXrEzCwgowPOIrG8EGkQ7ZZYrrE",
+  "authDomain": "thumbgenius-pz762.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "675902510186"
 };
 
 // Initialize Firebase
