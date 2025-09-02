ba7fc24d4c96814e918e396154e1085bbfd24612
subratami <subratamishra213@gmail.com>
2025-09-02 23:56:03 +0530

    update some changes in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,33 @@
+/*import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+*/
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -23,6 +53,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  turbopack: {
+    root: __dirname, // Sets the root to your workspace directory
   },
 };
 
